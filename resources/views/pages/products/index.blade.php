09f53c3040ae33b29f1abcd10730b7fb407507eb
Gabriel Meireles <gmeireles@2mundos.net>
2021-01-12 17:29:49 -0300

    Updating the products list view

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -5,53 +5,67 @@
     <h3 class="mb-0">Produtos</h3>
 </div>
 <div class="card-footer py-4">
-    <table class="table table-bordered">
-        <tr>
-            <td>
-                Nome do Produto
-            </td>
-
-            <td>
-                Tipo
-            </td>
-
-            <td>
-                Quantidade de caixas
-            </td>
-
-            <td>
-                Descrição
-            </td>
-
-            <td>
-                Data de validade
-            </td>
-        </tr>
-        <tbody>
-            @foreach ($products as $product)
+    @if(count($products) >= 1)
+        <table class="table table-bordered">
+            <thead class="thead-light">
                 <tr>
-                    <td>
-                        {{$product->name_prod}}
-                    </td>
-                    <td>
-                        {{$product->type_prod}}
-                    </td>
-                    <td>
-                        {{$product->qtd_box}}
-                    </td>
-                    <td>
-                        {{$product->description}}
-                    </td>
-                    <td>
-                        {{$product->validate_prod}}
-                    </td>
-                    <td>
-                        <a class="btn btn-outline-danger btn-sm" href="{{route('product.delete', $product->id)}}"><i class="fa fa-trash"></i></a>
-                        <a class="btn btn-outline-success btn-sm" href="{{route('product.edit', $product->id)}}"><i class="fa fa-edit"></i></a>
-                    </td>
+                    <th>
+                        Nome do Produto
+                    </th>
+
+                    <th>
+                        Tipo
+                    </th>
+
+                    <th>
+                        Quantidade de caixas
+                    </th>
+
+                    <th>
+                        Descrição
+                    </th>
+
+                    <th>
+                        Data de validade
+                    </th>
+                
+                    <th>
+                        Ações
+                    </th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($products as $product)
+                    <tr>
+                        <td>
+                            {{$product->name_prod}}
+                        </td>
+                        <td>
+                            {{$product->type_prod}}
+                        </td>
+                        <td>
+                            {{$product->qtd_box}}
+                        </td>
+                        <td>
+                            {{$product->description}}
+                        </td>
+                        <td>
+                            {{$product->validate_prod}}
+                        </td>
+                        <td>
+                            <a class="btn btn-outline-danger btn-sm" href="{{route('product.delete', $product->id)}}"><i class="fa fa-trash"></i></a>
+                            <a class="btn btn-outline-success btn-sm" href="{{route('product.edit', $product->id)}}"><i class="fa fa-edit"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <div class="col-lg-12">
+            <div class="alert alert-danger" role="alert">
+                Ainda não há produtos cadastrados
+            </div>
+        </div>
+    @endif
 </div>
 @endsection
