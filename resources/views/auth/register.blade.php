98e7f09129061528532a6ba59dbfe7e9c8cffb99
Gabriel Meireles <gmeireles@2mundos.net>
2021-01-13 17:54:55 -0300

    Updating the register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -15,8 +15,7 @@
             </div>
         </div>
         <div class="separator separator-bottom separator-skew zindex-100">
-            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1"
-                xmlns="http://www.w3.org/2000/svg">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
             </svg>
         </div>
@@ -26,29 +25,28 @@
             <div class="col-lg-7 col-md-7">
                 <div class="card bg-secondary border-0 mb-0">
                     <div class="card-body px-lg-5 py-lg-5">
-                    <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
                             <div class="row">
                                 <div class="col-lg-6">
                                     <label for="name" class="form-control-label pt-4">{{ __('Nome') }}</label>
-                                        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-                                        @error('name')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
 
                                 <div class="col-lg-6">
                                     <label for="email" class="form-control-label pt-4">{{ __('E-Mail') }}</label>
-                                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-                                        @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
                                 </div>
                             </div>
 
@@ -56,30 +54,35 @@
                                 <div class="col-lg-6">
                                     <label for="password" class="form-control-label pt-4">{{ __('Senha') }}</label>
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-                                    @error('password')
+                                        @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
-                                    @enderror
+                                        @enderror
                                 </div>
 
                                 <div class="col-lg-6">
                                     <label for="password-confirm" class="form-control-label pt-4">{{ __('Confirme a Senha') }}</label>
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                                    </div>
                                 </div>
                             </div>
+
 
                             <div class="row">
                                 <div class="col-lg-6">
                                     <label for="input-address" class="form-control-label pt-4">{{ __('Endereço') }}</label>
-                                    <input id="input-address" class="form-control" placeholder="Home Address" value="Rua: Direita, n° 88 , casa 4" type="text">
+                                    <input id="input-address" class="form-control @error('password') is-invalid @enderror" name="address" placeholder="Rua: Direita, n° 88 , casa 4" type="text">
+                                        @error('address')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                        @enderror
                                 </div>
 
                                 <div class="col-lg-6">
-                                    <label for="input-neighborhood" class="form-control-label pt-4" >{{ __('Bairro') }}</label>
-                                    <input type="text" id="input-neighborhood" class="form-control" placeholder="neighborhood" value="Vila Guilherme">
-                                    </div>
+                                    <label for="input-neighborhood" class="form-control-label pt-4">{{ __('Bairro') }}</label>
+                                    <input type="text" id="input-neighborhood" class="form-control @error('password') is-invalid @enderror" placeholder="neighborhood" value="Vila Guilherme">
+                                </div>
                             </div>
 
                             <div class="row">
@@ -114,7 +117,6 @@
                                 </div>
                             </div>
                         </form>
-                    </div>
                     </div>
                 </div>
             </div>
