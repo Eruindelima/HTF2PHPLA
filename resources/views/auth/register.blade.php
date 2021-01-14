ac7d14a00e5d5ae342c7798beed63adcda98fb19
Eruin de Lima Silva <48971348+Eruindelima@users.noreply.github.com>
2021-01-14 15:34:34 -0300

    Update register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -71,12 +71,12 @@
                             <div class="row">
                                 <div class="col-lg-6">
                                     <label for="input-country" class="form-control-label pt-4" >{{ __('CPF') }}</label>
-                                    <input type="text" id="inputCPF" class="form-control" placeholder="000.000.000-00" name="cpf">
+                                    <input type="text" id="inputCPF" class="form-control" placeholder="000.000.000-00" name="cpf" required="required">
                                 </div>
 
                                 <div class="col-lg-6">
                                     <label for="input-address" class="form-control-label pt-4">{{ __('Endereço') }}</label>
-                                    <input id="input-address" class="form-control @error('password') is-invalid @enderror" name="address" placeholder="Rua: Direita, n° 88 , casa 4" type="text">
+                                    <input id="input-address" class="form-control @error('password') is-invalid @enderror" name="address" placeholder="Rua: Direita, n° 88 , casa 4" type="text" required="required">
                                         @error('address')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -88,31 +88,31 @@
                             <div class="row">
                                 <div class="col-lg-6">
                                     <label for="input-neighborhood" class="form-control-label pt-4">{{ __('Bairro') }}</label>
-                                    <input type="text" id="input-neighborhood" class="form-control @error('password') is-invalid @enderror" placeholder="Santo Euardo" name="neighborhood">
+                                    <input type="text" id="input-neighborhood" class="form-control @error('password') is-invalid @enderror" placeholder="Santo Euardo" name="neighborhood" required="required">
                                 </div>
 
                                 <div class="col-lg-6">
                                     <label for="input-country" class="form-control-label pt-4" >{{ __('CEP') }}</label>
-                                    <input type="number" id="inputCEP" class="form-control" placeholder="00000-000" name="cep">
+                                    <input type="number" id="inputCEP" class="form-control" placeholder="00000-000" name="cep" required="required">
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-lg-6">
                                     <label for="input-city" class="form-control-label pt-4" >{{ __('Cidade') }}</label>
-                                    <input type="text" id="input-city" class="form-control" placeholder="Taboão da serra" name="city">
+                                    <input type="text" id="input-city" class="form-control" placeholder="Taboão da serra" name="city" required="required">
                                 </div>
 
                                 <div class="col-lg-6">
                                     <label for="input-country" class="form-control-label pt-4" >{{ __('Estado') }}</label>
-                                    <input type="text" id="input-state" class="form-control" placeholder="São paulo" name="state">
+                                    <input type="text" id="input-state" class="form-control" placeholder="São paulo" name="state" required="required">
                                 </div>
                             </div>
 
                         <div class="row">
                             <div class="col-lg-6">
                                 <label for="example-tel-input" class="form-control-label pt-4">{{ __('Telefone') }}</label>
-                                <input class="form-control" type="tel" placeholder="(11)-9.5050-5050" id="example-tel-input" name="phone">
+                                <input class="form-control" type="tel" placeholder="(11)-9.5050-5050" id="example-tel-input" name="phone" required="required">
                             </div>
                         </div>
 
