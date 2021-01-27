a0ca76ba21f44ea621316de60a07a16676d04ba8
Eruin de Lima Silva <48971348+Eruindelima@users.noreply.github.com>
2021-01-27 13:58:56 -0300

    Criando a interface de edição de um contato

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -11,6 +12,12 @@ class UserController extends Controller
     {
         $user_id = Auth::id();
         $profile = User::findOrFail($user_id);
+
+        $profile = DB::table('users')
+            ->join('user_contact', 'users.id', '=', 'user_contact.user_id')
+            ->select('*')
+            ->where('users.id', Auth::id())
+            ->first();
 
         return view('pages.profile.edit_profile', ['profile' => $profile]);
     }
--- resources/views/pages/profile/edit_profile.blade.php
+++ resources/views/pages/profile/edit_profile.blade.php
@@ -19,7 +19,7 @@
             </div>
             <div class="col-lg-6">
                 <label for="email" class="form-control-label pt-4">{{ __('E-Mail') }}</label>
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{!empty($profile->email) ? $profile->email : ''}}" required autocomplete="email" autofocus>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -30,7 +30,7 @@
 
         <div class="row">
             <div class="col-lg-6">
-                <label for="password" class="form-control-label pt-4">{{ __('Senha') }}</label>
+                <label for="password" class="form-control-label pt-4">{{ __('Digite sua nova senha') }}</label>
                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
                 @error('password')
                 <span class="invalid-feedback" role="alert">
@@ -40,29 +40,18 @@
             </div>
 
             <div class="col-lg-6">
-                <label for="password-confirm" class="form-control-label pt-4">{{ __('Confirme a Senha') }}</label>
+                <label for="password-confirm" class="form-control-label pt-4">{{ __('Confirme nova Senha') }}</label>
                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
             </div>
         </div>
-
-
-<<<<<<< HEAD
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <label for="input-country" class="form-control-label pt-4" >{{ __('CPF') }}</label>
-                                    <input type="text" id="inputCPF" class="form-control" placeholder="000.000.000-00" name="cpf" required="required" value="{{ $user->cpf }}" />
-                                </div>
-=======
         <div class="row">
             <div class="col-lg-6">
                 <label for="input-country" class="form-control-label pt-4">{{ __('CPF') }}</label>
-                <input type="text" id="inputCPF" class="form-control" placeholder="000.000.000-00" name="cpf" required="required">
+                <input type="text" id="inputCPF" class="form-control"  name="cpf" value="{{!empty($profile->cpf) ? $profile->cpf : ''}}" required autocomplete="cpf" autofocus>
             </div>
->>>>>>> 305c160d894f7058cdb5f2720c62e7c85cd20dab
-
             <div class="col-lg-6">
                 <label for="input-address" class="form-control-label pt-4">{{ __('Endereço') }}</label>
-                <input id="input-address" class="form-control @error('password') is-invalid @enderror" name="address" placeholder="Rua: Direita, n° 88 , casa 4" type="text" required="required">
+                <input id="input-address" class="form-control @error('password') is-invalid @enderror" name="address"  value="{{!empty($profile->address) ? $profile->address : '' }}" required autocomplete="address" >
                 @error('address')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
@@ -74,34 +63,39 @@
         <div class="row">
             <div class="col-lg-6">
                 <label for="input-neighborhood" class="form-control-label pt-4">{{ __('Bairro') }}</label>
-                <input type="text" id="input-neighborhood" class="form-control @error('password') is-invalid @enderror" placeholder="Santo Euardo" name="neighborhood" required="required">
+                <input type="text" id="input-neighborhood" class="form-control @error('password') is-invalid @enderror" name="neighborhood" value="{{!empty($profile->neighborhood) ? $profile->neighborhood : ''}}" required autocomplete="neighborhood" autofocus>
             </div>
 
             <div class="col-lg-6">
                 <label for="input-country" class="form-control-label pt-4">{{ __('CEP') }}</label>
-                <input type="number" id="inputCEP" class="form-control" placeholder="00000-000" name="cep" required="required">
+                <input type="number" id="inputCEP" class="form-control" name="cep" value="{{!empty($profile->cep) ? $profile->cep : ''}}" required autocomplete="cep" autofocus>
             </div>
         </div>
 
         <div class="row">
             <div class="col-lg-6">
                 <label for="input-city" class="form-control-label pt-4">{{ __('Cidade') }}</label>
-                <input type="text" id="input-city" class="form-control" placeholder="Taboão da serra" name="city" required="required">
+                <input type="text" id="input-city" class="form-control" name="city" value="{{!empty($profile->city) ? $profile->city : ''}}" required autocomplete="city" autofocus>
             </div>
 
             <div class="col-lg-6">
                 <label for="input-country" class="form-control-label pt-4">{{ __('Estado') }}</label>
-                <input type="text" id="input-state" class="form-control" placeholder="São paulo" name="state" required="required">
+                <input type="text" id="input-state" class="form-control" name="state" value="{{!empty($profile->state) ? $profile->state : ''}}" required autocomplete="state" autofocus>
             </div>
         </div>
 
         <div class="row">
             <div class="col-lg-6">
                 <label for="example-tel-input" class="form-control-label pt-4">{{ __('Telefone') }}</label>
-                <input class="form-control" type="tel" placeholder="(11)-9.5050-5050" id="example-tel-input" name="phone" required="required">
+                <input class="form-control" type="tel"  id="example-tel-input" name="phone" value="{{!empty($profile->phone) ? $profile->phone : ''}}" required autocomplete="phone" autofocus>
             </div>
         </div>
-
+        <div class="row">
+            <div class="col-lg-6">
+                <label for="image" class="form-control-label">{{ Auth::user()->name}} {{ __('Coloque uma foto em seu perfil') }}</label>
+                <input type="file" id="image" name="image" class="custom-file-input">
+            </div>
+        </div>
         <div class="form-group row mt-4">
             <div class="col-md-6 offset-md-4">
                 <button type="submit" class="btn btn-primary">
